909d788450386e613892e4fa27d452a991fcc5a8
smdsu <109036946+smdsu@users.noreply.github.com>
2025-09-05 10:17:58 +0300

    Fix import paths in migrations environment file to use absolute imports

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -6,8 +6,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from ..core.db_settings import db_settings
-from ..models import Base, Package, PackageType  # noqa: F401
+from src.core.db_settings import db_settings
+from src.models import Base, Package, PackageType  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
