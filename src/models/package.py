df9660e8d5f8e705fe37351966592d2a5cbc6421
smdsu <109036946+smdsu@users.noreply.github.com>
2025-09-05 12:30:19 +0300

    Update Package model to make package_delivery_cost_rub nullable and adjust user_session_uid mapping
    - Changed `package_delivery_cost_rub` to be nullable.
    - Updated `user_session_uid` mapping to remove default value.

--- src/models/package.py
+++ src/models/package.py
@@ -20,9 +20,9 @@ class Package(Base):
     type_id: Mapped[int] = mapped_column(ForeignKey("packagetypes.id"))
     weight: Mapped[float] = mapped_column(Float, nullable=False)
     value_of_contents_usd: Mapped[Decimal] = mapped_column(DECIMAL(10, 2))
-    package_delivery_cost_rub: Mapped[Decimal] = mapped_column(DECIMAL(10, 2))
-    user_session_uid: Mapped[uuid.UUID] = mapped_column(
-        PgUUID(as_uuid=True), default=uuid.uuid4
+    package_delivery_cost_rub: Mapped[Decimal | None] = mapped_column(
+        DECIMAL(10, 2), nullable=True
     )
+    user_session_uid: Mapped[uuid.UUID] = mapped_column(PgUUID(as_uuid=True))
 
     type: Mapped["PackageType"] = relationship(back_populates="packages")
